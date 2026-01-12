db3d37fb48cdcbcb6b415c66e973cdea0bb9201d
InchanHan <inchan5213@gmail.com>
2026-01-13 03:16:40 +0900

    add: added Duration, and TimeUnit for time deref

--- chronix/src/timer.rs
+++ chronix/src/timer.rs
@@ -1,6 +1,60 @@
 use std::sync::OnceLock;
 use std::time::Instant;
 
+pub struct Duration {
+    value: f64,
+    unit: TimeUnit,
+}
+
+impl Duration {
+    pub fn from_ns(ns: f64) -> Self {
+        let unit = TimeUnit::auto_from_ns(ns);
+        Self {
+            value: ns / unit.scale_ns(),
+            unit,
+        }
+    }
+}
+
+pub enum TimeUnit {
+    Ns,
+    Us,
+    Ms,
+    S,
+}
+
+impl TimeUnit {
+    pub fn suffix(&self) -> &'static str {
+        match self {
+            TimeUnit::Ns => "ns",
+            TimeUnit::Us => "µs",
+            TimeUnit::Ms => "ms",
+            TimeUnit::S => "s",
+        }
+    }
+
+    pub fn scale_ns(&self) -> f64 {
+        match self {
+            TimeUnit::Ns => 1.0,
+            TimeUnit::Us => 1000.0,
+            TimeUnit::Ms => 1_000_000.0,
+            TimeUnit::S => 1_000_000_000.0,
+        }
+    }
+
+    pub fn auto_from_ns(ns: f64) -> Self {
+        if ns < 1_000.0 {
+            TimeUnit::Ns
+        } else if ns < 1_000_000.0 {
+            TimeUnit::Us
+        } else if ns < 1_000_000_000.0 {
+            TimeUnit::Ms
+        } else {
+            TimeUnit::S
+        }
+    }
+}
+
 pub trait Timer {
     fn now(&self) -> u64;
     fn barrier_before(&self) {}
